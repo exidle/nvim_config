1cdc37f88af53b36081c379d58ba21a7dd005efa
Oleksandr Pushkar <alexanderpushkar86@gmail.com>
2024-09-28 10:13:39 +0200

    Commit on 28/09 10:13
    Change-Id: I0b8eeb47a88faa3c58d0eddde05f163b52687e28

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,27 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-	  "nvim-lua/plenary.nvim",
-	  "MunifTanjim/nui.nvim"
-  },
-    keys = {
-      { "<leader>tt", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-    },
-    config = function()
-      require("neo-tree").setup()
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim"
+	},
+	keys = {
+		{ "<leader>tt", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+	},
+	opts = {
+		sources = {"filesystem", "buffers", "git_status" },
+		open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+		filesystem = {
+			bind_to_cwd = false,
+			follow_current_file = { enabled = true },
+		},
+		default_component_settnigs = {
+			git_status = {
+				symbols = {
+				        unstaged = "󰄱",
+					staged = "󰱒",
+				},
+			},
+		},
+	},
 }
